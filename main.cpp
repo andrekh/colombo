2890387d28300df81cfa5126f4d1303d65433ad6
Andre Khoury <khoury.andre@protonmail.com>
2021-07-23 09:36:16 -0400

    hostname resolve

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "utils.h"
 
 int main(int argc, char *argv[]) {
 
@@ -13,16 +14,17 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    char *targetIP = argv[1];
+    char *target = get_IP_from_hostname(argv[1]);
     int startPort = atoi(argv[2]);
     int endPort = atoi(argv[3]);
 
     struct sockaddr_in target_addr = {0};
     int sock = 0;
 
-    target_addr.sin_family = AF_INET;
+    target_addr.sin_family = AF_INET;   //IPv4
 
-    if (inet_pton(AF_INET, targetIP, &target_addr.sin_addr) <= 0) {
+    // inet_pton converts IPv4 adresses from text to binary form
+    if (inet_pton(AF_INET, target, &target_addr.sin_addr) <= 0) {   //inet_pton copies the target ip to target_addr.sin_addr
         std::cout << "Error converting target ip address to binary form." << std::endl;
         return -1;
     }
